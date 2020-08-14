5147543817ec8c16e526505a6326d042b6e05d83
Cameron Rouse <cam-rouse@hotmail.co.uk>
2020-08-14 12:55:48 +0100

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+
   root to: 'cocktails#index'
 
   resources :cocktails do
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   resources :reviews
 end
   resources :doses, only: [:destroy]
-=======
->>>>>>> parent of 6567238... cocktail. models and attributes
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
