4ce51ceb5ae611da2294182744170a84a9d358d3
Cameron Rouse <cam-rouse@hotmail.co.uk>
2020-08-14 11:26:13 +0100

    Add Article scaffold

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,8 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+  validates_uniqueness_of :cocktail_id, :scope => [:ingredient_id]
+  validates :description, presence: true
+
+  # validates :cocktail, presence: true, uniqueness: {scope: :ingredient, :cocktail, message: 'pairings should be unique' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  resources :cocktails
+
+  resources :articles
+  root to: 'cocktails#index'
+
+  root to: 'articles#index'
+  resources :articles, except: :index
+
+  resources :cocktails do
+  resources :doses, except: [:destroy]
+  resources :reviews
+end
+  resources :doses, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
